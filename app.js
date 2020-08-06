a67a409470cc572dbd5625d8bcbb3a9be4267d8d
Shreyas Bhandare <bhandare.shreyas@gmail.com>
2020-08-06 13:11:23 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+const { describe, argv, demand } = require('yargs')
+
+// Customier yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Description',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove the note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () {
+        notes.removeNote(argv.title)
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        notes.listNotes()
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
